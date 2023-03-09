d5432f03900b2371900c02c727b0fceaa219153f
Ritvik Nag <nagrim7@gmail.com>
2023-03-09 01:38:31 -0500

    Bump version: 0.1.1 → 0.1.2

--- profile_photo/__version__.py
+++ profile_photo/__version__.py
@@ -5,7 +5,7 @@ Profile Photo - Center + Crop Image to create a Profile Pic or Headshot
 __title__ = 'profile-photo'
 __description__ = 'Center + Crop Image to create a Profile Pic or Headshot'
 __url__ = 'https://github.com/rnag/profile-photo'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Ritvik Nag'
 __author_email__ = 'rv.kvetch@gmail.com'
 __license__ = 'MIT'
